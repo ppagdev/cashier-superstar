ca78625c0f2ea741f1a0b80f23181e9ac6931a0f
Ppag <ppagdev@tuta.com>
2024-02-14 14:32:52 +0000

    added milk carton and collision shapes

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
 var bread_scene = preload("res://scenes/bread.tscn")
-var treadmill_speed = 200
+var milk_carton_scene = preload("res://scenes/milk_carton.tscn")
+
+var treadmill_speed = 100
+var spawn_time = 3
 var instanstiated_items = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	create_item(bread_scene)
+	item_spawner()
 
 func create_item(item_scene):
 	var item = item_scene.instantiate()
@@ -14,7 +17,21 @@ func create_item(item_scene):
 	add_child(item)
 	instanstiated_items.append(item)
 
+func choose_item():
+	var random_float = randf()
+	if random_float < 0.5:
+		return bread_scene
+	else:
+		return milk_carton_scene
+
+func item_spawner():
+	while true:
+		var item_scene = choose_item()
+		create_item(item_scene)
+		await get_tree().create_timer(spawn_time).timeout
+
 func treadmill(delta):
+	# move items
 	for item in instanstiated_items:
 		item.position += Vector2(treadmill_speed * delta,  0)
 
